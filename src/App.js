1e0904a03b4ca12ae00baa9dd2cac1b8e723da8c
Afroz quraishi <afroziiitian@gmail.com>
2021-03-05 15:42:28 +0530

    ChatFeed added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+function App(){
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='afroz23'
+			userSecret='12345'
+			projectID='23710331-c1be-43b2-86ba-3a44198726ea'
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps }/>}
+        />
+	);
 }
-
 export default App;
---
+++ src/components/TheirMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const TheirMessage = ({lastMessage,message}) =>{
+
+    return (
+        <div>
+            TheirMessage
+        </div>
+    );
+}
+export default TheirMessage;
+
+
