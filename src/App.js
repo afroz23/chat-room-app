62aa45bf77e4b06e6cb9e8e93b4e8b5f3206fcfc
Afroz quraishi <afroziiitian@gmail.com>
2021-03-12 11:42:33 +0530

    Login Form added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 import './App.css';
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
-function App(){
+import LoginForm from './components/LoginForm';
+
+const projectID = '23710331-c1be-43b2-86ba-3a44198726ea';
+
+const App = () => {
+	if (!localStorage.getItem('username')) return <LoginForm />;
+  
 	return (
-		<div>
-		<ChatEngine
-			height='100vh'
-			userName='hritik99'
-			userSecret='12345'
-			projectID='23710331-c1be-43b2-86ba-3a44198726ea' 
-        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps }/>}
-        />
-		</div>
+	  <ChatEngine
+		height="100vh"
+		projectID={projectID}
+		userName={localStorage.getItem('username')}
+		userSecret={localStorage.getItem('password')}
+		renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+		onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+	  />
 	);
-}
+  };
 export default App;
