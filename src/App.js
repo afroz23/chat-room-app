a9d81a0db4ae95975dce06f7f843e4e4f9a1e922
Afroz quraishi <afroziiitian@gmail.com>
2021-03-12 10:55:44 +0530

    components are modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './App.css';
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
 function App(){
 	return (
+		<div>
 		<ChatEngine
 			height='100vh'
-			userName='afroz23'
+			userName='hritik99'
 			userSecret='12345'
-			projectID='23710331-c1be-43b2-86ba-3a44198726ea'
+			projectID='23710331-c1be-43b2-86ba-3a44198726ea' 
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps }/>}
         />
+		</div>
 	);
 }
 export default App;
